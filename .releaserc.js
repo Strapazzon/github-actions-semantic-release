cb864b3d14c33cb94c14fdbd6d39afcc2f8ace98
Getulio <strapazzon09@gmail.com>
2020-04-02 09:43:52 -0300

    feat(add changelog.md): add ChangeLog.md

--- .releaserc.js
+++ .releaserc.js
@@ -3,11 +3,16 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md",
+    }],
     ['@semantic-release/npm', { 
       npmPublish: false, 
       allowSameVersion: true
     }],
-    '@semantic-release/github',
+    ['@semantic-release/github', {
+      "assets": ["CHANGELOG.md"],
+    }],
     ["@semantic-release/exec", {
       prepareCmd : "sh ./prepare.sh ${nextRelease.version}"
     }]
